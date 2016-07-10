e6b98de8ee80a01b1f00bc706b40d48cc1d6807f
Brian Ethier <davidbrianethier@gmail.com>
2016-07-10 00:20:14 -0400

    changing the report timeout to 10 min

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -2,7 +2,7 @@ class ReportsController < ApplicationController
   def create
     #Get reports from same IP within an hour
     #TODO: CHange this back from 10 seconds when going to prod
-    previous_reports = Report.where(ip: request.remote_ip, time: 10.seconds.ago..DateTime.now)
+    previous_reports = Report.where(ip: request.remote_ip, time: 10.minutes.ago..DateTime.now)
 
     if previous_reports.any?
       return head :too_many_requests
